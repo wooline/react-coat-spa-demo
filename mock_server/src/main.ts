52f025183aaeb62b6520dd60e2991aa0700b6f5f
jimmy.zhao <wooline@dingtalk.com>
2018-03-26 18:39:28 +0800

    去掉mock

--- mock_server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import {NestFactory} from "@nestjs/core";
-import {UserModule} from "./user/user.module";
-
-async function bootstrap() {
-    const app = await NestFactory.create(UserModule);
-    await app.listen(3000);
-}
-bootstrap();
