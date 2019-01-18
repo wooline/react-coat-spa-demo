a1f0b325f3126825d73b4f8fab681df6845a08d0
jimmy.zhao <wooline@dingtalk.com>
2019-01-18 16:09:45 +0800

    修改文档

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,7 @@ import {moduleGetter} from "modules";
 import {ModuleNames} from "modules/names";
 import {buildApp} from "react-coat";
 
-const env = getInitEnv();
-const initEnv: typeof InitEnv = {
-  clientPublicPath: env.clientPublicPath,
-  apiServerPath: env.apiServerPath,
-};
-// tslint:disable-next-line:no-string-literal
-window["InitEnv"] = initEnv;
+// 获取全局设置的函数，为了在上线后可以由运维修改，该函数的实现放在/public/index.html中，以防止被 webpack 打包
+window.InitEnv = getInitEnv();
 
 buildApp(moduleGetter, ModuleNames.app, {routerParser});
